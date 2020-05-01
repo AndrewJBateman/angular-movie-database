e3fb726ede12a76c570eb7b720c793816bd1cf06
Andrew Bateman <gomezbateman@yahoo.com>
2020-05-01 21:00:07 +0200

    dependencies updated, prettier formatting

--- src/app/movie/movie-detail/movie-detail.component.ts
+++ src/app/movie/movie-detail/movie-detail.component.ts
@@ -1,14 +1,14 @@
-import { NavbarService } from './../../navbar/services/navbar.service';
-import { MovieService } from './../services/movie.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { Movie } from '../models/movie.model';
+import { NavbarService } from "./../../navbar/services/navbar.service";
+import { MovieService } from "./../services/movie.service";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
+import { Movie } from "../models/movie.model";
 
 @Component({
-  selector: 'app-movie-detail',
-  templateUrl: './movie-detail.component.html',
-  styleUrls: ['./movie-detail.component.scss']
+  selector: "app-movie-detail",
+  templateUrl: "./movie-detail.component.html",
+  styleUrls: ["./movie-detail.component.scss"],
 })
 export class MovieDetailComponent implements OnInit, OnDestroy {
   id: number;
@@ -18,21 +18,20 @@ export class MovieDetailComponent implements OnInit, OnDestroy {
   constructor(
     private movieService: MovieService,
     private navbarService: NavbarService,
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    this.id = +this.route.snapshot.paramMap.get('id');
+    this.id = +this.route.snapshot.paramMap.get("id");
     this.movieSub$ = this.movieService
       .movieFromHttp(this.id)
-      .subscribe(movie => {
-      this.movie = movie;
-      this.navbarService.title.next(movie.name);
-    });
+      .subscribe((movie) => {
+        this.movie = movie;
+        this.navbarService.title.next(movie.name);
+      });
   }
 
   ngOnDestroy(): void {
     this.movieSub$.unsubscribe();
   }
-
 }
--- src/app/movie/movie-routing.module.ts
+++ src/app/movie/movie-routing.module.ts
@@ -1,29 +1,27 @@
-import { AddMovieComponent } from './add-movie/add-movie.component';
-import { MovieListComponent } from './movie-list/movie-list.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { AddMovieComponent } from "./add-movie/add-movie.component";
+import { MovieListComponent } from "./movie-list/movie-list.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MovieDetailComponent } from "./movie-detail/movie-detail.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: MovieListComponent,
   },
   {
-    path: 'add',
+    path: "add",
     component: AddMovieComponent,
   },
   {
-    path: ':id',
+    path: ":id",
     component: MovieDetailComponent,
-  }
+  },
 ];
 
 // it is specified that these are child routes
 @NgModule({
-  imports: [
-    RouterModule.forChild(routes)
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class MovieRoutingModule { }
+export class MovieRoutingModule {}
